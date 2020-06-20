465248bd4abee33c70945607f827538765b0f3a0
gauti21 <gautamsoumitra.21@gmail.com>
2020-06-20 15:06:33 +0530

    1.1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank'
 import Particles from 'react-particles-js';
 import './App.css';
 import 'tachyons';
+import Clarifai from 'clarifai';
 
+const app = new Clarifai.App({
+  apiKey: '674fa520097e4f9389be7e9e95f79450'
+ });
 
 const particlesOptions = {
   particles: {
@@ -25,16 +30,42 @@ class App extends React.Component {
   constructor(){
     super();
     this.state={
-      input: ''
+      input: '',
+      imageUrl: '',
+      box: {}
     }
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageUrl: this.state.input});
+    //console.log('click');
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+     .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+     .catch(err => console.log(err));
   }
 
   render() {
@@ -51,7 +82,7 @@ class App extends React.Component {
       onInputChange={this.onInputChange}
       onButtonSubmit={this.onButtonSubmit}
       />
-
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
     
     </div>
@@ -60,3 +91,8 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+      //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      // do something with response
+      //console.log(response);
